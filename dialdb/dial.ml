b8455ffdf3b2622d1247dd855a96fd23b0d83f55
Moreno PEDRO <nostdrelease@gmail.com>
2014-11-03 13:43:42 -0200

    update of mli file for using ocamldoc, edition of line 36 && 31

--- dialdb/dial.ml
+++ dialdb/dial.ml
@@ -1,34 +1,29 @@
 (* pedro_m *)
-
 module type DIAL =
   sig
-    (** store the request for easy access use by name **)
     val prepare_requests : (string * string) list list -> unit
-    (** clean stored request **)
     val clean_request : ('a * string) list list -> unit
-    (** execute specific request by name with parameters **)
     val execute_request : (string * string option list) -> string list
-    (** draw the row **)
     val print_row : string option list -> unit
-    (** close the access to the database **)
     val close : unit -> unit
-			  
   end
 
 module Dial : DIAL =
   struct
 
-    let dbh = PGOCaml.connect ~database:"oaethernity_db" ()
+    let dbh = PGOCaml.connect ~database:"DB_NAME" ()
 			      
     let rec print_row = function
-      | Some o :: next -> print_string (o ^ " ");print_row next
-      | None :: next -> print_string ("-" ^ " ");print_row next
+      | Some o :: next -> print_string (o ^ " "); print_row next
+      | None :: next -> print_string ("-" ^ " "); print_row next
       | _ -> print_endline ""
 			   
     let prepare_requests = fun rql -> List.iter (fun l -> List.iter (fun (req, name) -> PGOCaml.prepare dbh ~query:req ~name:name ()) l) rql
+
     let clean_request = fun rql -> List.iter (fun l -> List.iter (fun ( _, name) -> PGOCaml.close_statement dbh ~name:name ()) l) rql
+
     let execute_request = fun (name, params) ->
-      let r = (PGOCaml.execute dbh ~name:name ~params:params ()) in
+      let r = PGOCaml.execute dbh ~name:name ~params:params () in
       List.iter print_row r;
       List.map (function | Some o :: next -> o | _ -> "") r
 								
--- dialdb/dial.mli
+++ dialdb/dial.mli
@@ -1,14 +1,24 @@
 (* pedro_m *)
-
+(** DIAL signature for implementing dial between an application and a database *)
 module type DIAL =
   sig
-
+    
+    (** Store the request for easy access call, using by name *)
     val prepare_requests : (string * string) list list -> unit
+
+    (** Clean all stored requests *)
     val clean_request : ('a * string) list list -> unit
+
+    (** Execute specific request by name with parameters *)
     val execute_request : (string * string option list) -> string list
+
+    (** Draw a resulting row *)
     val print_row : string option list -> unit
+
+    (** Close the access to the database *)
     val close : unit -> unit
 			  
   end
 
+(** Dial is a module implementing DIAL signature using PGOcaml functions *)
 module Dial : DIAL 
